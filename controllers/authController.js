77c64919160561d8ccf884c36555c65f4b34ba0a
VarunVerma09 <umangsoni409@gmail.com>
2025-09-27 22:39:41 +0530

    Ecommerce Backend Setup

---
+++ controllers/authController.js
@@ -0,0 +1,36 @@
+import userModel from "../models/userModel.js";
+
+
+export const registerController = async (req,res)=>{
+    try {
+        const {name,email,password,phone,address} = req.body;
+        //validaion
+        if(!name){
+            return res.send({error:"Name is Required"});
+        }
+         if(!email){
+            return res.send({error:"Email is Required"});
+        }
+         if(!password){
+            return res.send({error:"Password is Required"});
+        }
+         if(!phone){
+            return res.send({error:"Phone is Required"});
+        }
+         if(!address){
+            return res.send({error:"Address is Required"});
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            massage:"Error in Registration",
+            error
+        })
+        
+    }
+
+};
+
+
