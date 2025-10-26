a21ddf7f48bb6798fd2f59de2ee1d534f15db3f9
VarunVerma09 <umangsoni409@gmail.com>
2025-10-26 22:21:34 +0530

    completed

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use("/api/v1/category", categoryRoute);
 app.use("/api/v1/product", productRoute);
 
 // ✅ Handle SPA routes
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
 });
 
